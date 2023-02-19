f21ad290357be6b1fec8fcfeb39fd7f9400715d7
Jakub Sekula <seklerek@gmail.com>
2023-02-19 23:33:20 +0000

    Style adjustments

--- components/home/SectionContainer.jsx
+++ components/home/SectionContainer.jsx
@@ -5,7 +5,7 @@ export default function SectionContainer({ title = "title" ,children}) {
     <motion.section
       // {...sectionEntryAnimation}
       id={`${title}-section`}
-      className="mx-auto flex w-full max-w-page flex-col items-center gap-12 my-16 px-4 xl:px-0 overflow-hidden"
+      className="mx-auto flex w-full max-w-page flex-col items-center gap-12 my-16 px-6 xl:px-4 2xl:px-0 overflow-hidden"
     >
       <h2 id="webdev" className="font-heading text-4xl font-semibold ">
         {title}
