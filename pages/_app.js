b312f1ca85d8fb80ac7aa3c34c3353aa45d29268
Jakub Sekula <seklerek@gmail.com>
2023-05-15 23:41:24 +0100

    Remove react-parallax

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,23 @@
 import "/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { LayoutProvider } from "components/contexts";
-import { ParallaxProvider } from "react-scroll-parallax";
 import { useEffect } from "react";
 import { reveal } from "lib/utils";
 
 export default function App({ Component, pageProps, router }) {
   useEffect(() => {
-    window.addEventListener("scroll", reveal);
-
-    // To check the scroll position on page load
     reveal();
+    window.addEventListener("scroll", reveal);
+    // To check the scroll position on page load
     return () => {
       window.removeEventListener("scroll", reveal);
     };
   }, []);
+  
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <LayoutProvider>
-        <ParallaxProvider>
           <Component {...pageProps} key={router.asPath} />
-        </ParallaxProvider>
       </LayoutProvider>
     </ThemeProvider>
   );
