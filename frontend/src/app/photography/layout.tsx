74ed9dc65be8fce801073010a5037a9229210d5e
Jakub Sekula <seklerek@gmail.com>
2023-07-15 14:07:18 +0100

    Add layouts to all pages

--- frontend/src/app/photography/layout.tsx
+++ frontend/src/app/photography/layout.tsx
@@ -12,62 +12,29 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [menuItems, data] = await Promise.all([getMenuItems(), getData()]) 
+  const data = await getData();
   return (
-    <div className="flex h-full min-h-screen w-screen ">
-      <SidebarNavigation menuItems={menuItems} data={data}/>
-      <main className="flex w-full h-full flex-col overflow-scroll">{children}</main>
+    <div className="flex h-full min-h-screen">
+      <SidebarNavigation data={data} />
+      <main className="flex h-full w-full md:max-w-[80%] flex-col ">
+        {children}
+      </main>
     </div>
   );
 }
 
-async function getMenuItems() {
-  const qs = require("qs");
-
-  let menuQuery = qs.stringify({
-    populate: "links",
-    filters: {
-      name: {
-        $eq: "Header",
-      },
-    },
-  });
-
-  let menu = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/menus?${menuQuery}`
-  );
-
-  const menuJson = await menu.json();
-
-  console.log(menuJson.data[0].attributes.links);
-
-  return menuJson.data[0].attributes.links;
-}
-
 async function getData() {
-  const qs = require("qs");
-
-  const strapiHeaders = new Headers({
+  const headers = new Headers({
     Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
   });
 
-  let strapiQuery = qs.stringify({
-    populate: ["featured_image"],
-    filters: {
-      showAsCategory: {
-        $eq: true,
-      },
-    },
+  let res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/albums`, {
+    headers: headers,
   });
 
-  let strapiRes = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/albums?${strapiQuery}`,
-    { headers: strapiHeaders }
-  );
-
-  let strapiResJson = await strapiRes.json();
+  let json = await res.json();
 
   return {
-    data: strapiResJson.data,
+    data: json.data,
   };
 }
