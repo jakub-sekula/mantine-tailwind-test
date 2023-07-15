dcdb2eda0a238909666339a70b8c9854454bb5b9
Jakub Sekula <seklerek@gmail.com>
2023-07-15 14:06:46 +0100

    Add Strapi backend to repository

---
+++ backend/src/api/tool-collection/controllers/tool-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tool-collection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tool-collection.tool-collection');
