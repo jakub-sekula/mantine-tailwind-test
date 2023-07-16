6511fbdd6b8fdabcff23a9b7e9cf2fabfc573e5e
Jakub Sekula <seklerek@gmail.com>
2023-07-16 19:12:43 +0100

    Fix exception when large image size not available

--- frontend/src/app/photography/[category]/page.jsx
+++ frontend/src/app/photography/[category]/page.jsx
@@ -18,11 +18,30 @@ export default async function Page({ params }) {
       >
         <div className="absolute inset-0 z-10 h-full bg-gradient-to-tr from-darkbg/90" />
         <Image
-          width={data.featured_image.data.attributes.formats.large.width}
-          height={data.featured_image.data.attributes.formats.large.height}
+          width={
+            data.featured_image.data.attributes.formats?.large?.width ||
+            data.featured_image.data.attributes.formats?.medium?.width ||
+            data.featured_image.data.attributes.formats?.small?.width ||
+            data.featured_image.data.attributes.formats?.xsmall?.width ||
+            data.featured_image.data.attributes.formats?.thumbnail?.width ||
+            300
+          }
+          height={
+            data.featured_image.data.attributes.formats?.large?.height ||
+            data.featured_image.data.attributes.formats?.medium?.height ||
+            data.featured_image.data.attributes.formats?.small?.height ||
+            data.featured_image.data.attributes.formats?.xsmall?.height ||
+            data.featured_image.data.attributes.formats?.thumbnail?.height ||
+            300
+          }
           alt={data.title}
           src={convertRelativeUrl(
-            data.featured_image.data.attributes.formats.large.url
+            data.featured_image.data.attributes.formats?.large?.url ||
+              data.featured_image.data.attributes.formats?.medium?.url ||
+              data.featured_image.data.attributes.formats?.small?.url ||
+              data.featured_image.data.attributes.formats?.xsmall?.url ||
+              data.featured_image.data.attributes.formats?.thumbnail?.url ||
+              "/default.jpg"
           )}
           priority={true}
           className="absolute inset-0 z-0 mx-auto h-full w-full object-cover"
@@ -60,105 +79,120 @@ export default async function Page({ params }) {
 }
 
 async function getData(params) {
-  try {
-    const qs = require("qs");
-    const { category } = params;
+  const qs = require("qs");
+  const { category } = params;
 
-    let query;
+  let query;
 
-    const headers = new Headers({
-      Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-    });
+  const headers = new Headers({
+    Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+  });
 
-    query = qs.stringify({
-      filters: {
-        slug: {
-          $eq: category,
-        },
+  query = qs.stringify({
+    filters: {
+      slug: {
+        $eq: category,
       },
-    });
+    },
+  });
 
-    const idRes = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/albums?${query}`,
-      {
-        headers,
-        next: { revalidate: 10 },
-      }
-    );
-    const idJson = await idRes.json();
+  const idRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/albums?${query}`,
+    {
+      headers,
+      next: { revalidate: 10 },
+    }
+  );
+  const idJson = await idRes.json();
 
-    const id = idJson?.data[0]?.id;
+  const id = idJson?.data[0]?.id;
 
-    query = qs.stringify({
-      populate: {
-        sections: {
-          tags: "*",
-          populate: {
-            gallery: true,
-            featured_image: true,
-            albums: {
-              populate: {
-                featured_image: "*",
-              },
+  query = qs.stringify({
+    populate: {
+      sections: {
+        tags: "*",
+        populate: {
+          gallery: true,
+          featured_image: true,
+          albums: {
+            populate: {
+              featured_image: "*",
             },
           },
         },
-        featured_image: {
-          populate: "formats",
-        },
       },
-    });
+      featured_image: {
+        populate: "formats",
+      },
+    },
+  });
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/albums/${id}?${query}`,
-      {
-        headers,
-        next: { revalidate: 10 },
-      }
-    );
-
-    const resJson = await res.json();
-
-    const data = resJson.data.attributes;
-
-    let imageLinks = [];
-
-    if (!!data.sections) {
-      data.sections.forEach((section) => {
-        if (
-          section.__component === "album.sections" &&
-          !!section.gallery?.data
-        ) {
-          section.gallery.data.forEach((item) => {
-            // generate a srcset from all strapi formats
-            const sizes = Object.keys(item.attributes.formats).map((key) => {
-              const format = item.attributes.formats[key];
-              return {
-                size: key,
-                src: convertRelativeUrl(format.url),
-                width: format.width,
-                height: format.height,
-              };
-            });
-
-            imageLinks.push({
-              id: item.id,
-              src: convertRelativeUrl(item.attributes.formats.xlarge.url),
-              width: item.attributes.formats.xlarge.width,
-              height: item.attributes.formats.xlarge.height,
-              srcSet: sizes,
-              description: item.attributes.caption,
-            });
-          });
-        }
-      });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/albums/${id}?${query}`,
+    {
+      headers,
+      next: { revalidate: 10 },
     }
+  );
 
-    return {
-      data,
-      imageLinks,
-    };
-  } catch (err) {
-    notFound();
+  const resJson = await res.json();
+
+  const data = resJson.data.attributes;
+
+  let imageLinks = [];
+
+  if (!!data.sections) {
+    data.sections.forEach((section) => {
+      if (section.__component === "album.sections" && !!section.gallery?.data) {
+        section.gallery.data.forEach((item) => {
+          // generate a srcset from all strapi formats
+          const sizes = Object.keys(item.attributes.formats).map((key) => {
+            const format = item.attributes.formats[key];
+            return {
+              size: key,
+              src: convertRelativeUrl(format.url),
+              width: format.width,
+              height: format.height,
+            };
+          });
+
+          imageLinks.push({
+            id: item.id,
+            src: convertRelativeUrl(
+              item.attributes.formats?.xlarge?.url ||
+                item.attributes.formats?.large?.url ||
+                item.attributes.formats?.medium?.url ||
+                item.attributes.formats?.small?.url ||
+                item.attributes.formats?.xsmall?.url ||
+                item.attributes.formats?.thumbnail?.url ||
+                "/default.jpg"
+            ),
+            width:
+              item.attributes.formats?.xlarge?.width ||
+              item.attributes.formats?.large?.width ||
+              item.attributes.formats?.medium?.width ||
+              item.attributes.formats?.small?.width ||
+              item.attributes.formats?.xsmall?.width ||
+              item.attributes.formats?.thumbnail?.width ||
+              300,
+            height:
+              item.attributes.formats?.xlarge?.height ||
+              item.attributes.formats?.large?.height ||
+              item.attributes.formats?.medium?.height ||
+              item.attributes.formats?.small?.height ||
+              item.attributes.formats?.xsmall?.height ||
+              item.attributes.formats?.thumbnail?.height ||
+              300,
+            srcSet: sizes,
+            description: item.attributes.caption,
+          });
+        });
+      }
+    });
   }
+
+  return {
+    data,
+    imageLinks,
+  };
 }
