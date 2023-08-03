96ee4d137d76d04c36c3c71eeef7aee520e625c1
Jakub Sekula <seklerek@gmail.com>
2023-08-04 00:12:10 +0100

    Fix homepage overflow on movile

--- frontend/src/components/home/SectionContainer.jsx
+++ frontend/src/components/home/SectionContainer.jsx
@@ -3,7 +3,7 @@ export default function SectionContainer({ title = "", children }) {
     <section
       id={`${title}-section`}
       className="reveal fade-bottom mx-auto mb-8 md:mb-32 flex w-full max-w-page flex-col
-      items-center gap-4 px-6 last:mb-0 md:gap-12 xl:px-4 2xl:px-0"
+      items-center gap-4 px-6 last:mb-0 md:gap-12 xl:px-4 2xl:px-0 overflow-hidden md:overflow-visible"
     >
       {!!title ? (
         <h2
